9a34139f82e43aa132e4d7537dcd088bfb801404
Justin Sams <justinsams98@yahoo.de>
2021-08-27 13:13:14 +0200

    minor changes to predictor.py

--- tests/test_predictor.py
+++ tests/test_predictor.py
@@ -10,21 +10,12 @@ def testing_load_img_shape():
     assert predictor.load_img("raw_data/test_pic.jpg").shape == [placeholder[0], placeholder[1], 3]
 
 
-def testing_predict_return_list():
+def testing_predict():
     assert type(predictor.predict("raw_data/test_pic.jpg")) == list
-
-
-def testing_predict_return_str():
     assert type(predictor.predict("raw_data/test_pic.jpg")[0]) == str
 
 
-def testing_run_detector_return_tuple():
+def testing_run_detector():
     assert type(predictor.run_detector("raw_data/test_pic.jpg")) == tuple
-
-
-def testing_run_detector_return_bytes():
     assert type(predictor.run_detector("raw_data/test_pic.jpg")[0][0]) == bytes
-
-
-def testing_run_detector_return_numpyfloat32():
     assert type(predictor.run_detector("raw_data/test_pic.jpg")[1][0]) == numpy.float32
