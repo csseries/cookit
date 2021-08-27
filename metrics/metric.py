d0bd931a4cd78d1e307b42fdca379969312bdfc9
Justin Sams <justinsams98@yahoo.de>
2021-08-27 17:11:31 +0200

    metric.py file creates new folder in project and downloads all the pictures from the csv file. The links and lists of foods are split and transformed into a dict with keys 0,1,2,...

--- metrics/metric.py
+++ metrics/metric.py
@@ -1,28 +1,53 @@
 import csv
 from cookit.predict import Predictor
 import numpy
+import requests
+import os
 
-
-csv_list = []
-link_list = []
-food_list = []
 
 predictor = Predictor()
 
 
-with open('food_testing_set.csv') as csvfile:
-    read = csv.reader(csvfile, delimiter=";")
-    for row in read:
-        csv_list.append(row)
+def retrieve_info_from_csv():
+    csv_list = []
+    link_list = []
+    food_list = []
 
-for row in csv_list:
-    link_list.append(row[0])
-    food_list.append(row[1:])
+    with open('food_testing_set.csv') as csvfile:
+        read = csv.reader(csvfile, delimiter=";")
+        for row in read:
+            csv_list.append(row)
 
-link_list.pop(0)
-food_list.pop(0)
+    for row in csv_list:
+        link_list.append(row[0])
+        food_list.append(row[1:])
+
+    link_list.pop(0)
+    food_list.pop(0)
+
+    return link_list, food_list
+
+def download_test_images(link_list):
+    counter = 0
+
+    for url in link_list:
+        image = requests.get(url).content
+
+        with open(f'images_from_csv/{counter}.jpg', 'wb') as writer:
+            writer.write(image)
+
+    counter += 1
 
 
-def calculating_accuracy():
+def making_prediction():
+    prediction_dict = {}
+    counter = 0
 
-    prediction = predictor.predict()
+    for pic in os.listdir("images_from_csv"):
+        prediction = predictor.predict(pic)
+        prediction_dict[f"{counter}"] = prediction
+
+    counter += 1
+
+
+making_prediction()
