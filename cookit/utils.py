6252a884a2f23836ecf584074775773f2b502874
Michael Weitnauer <michael.weitnauer@gmail.com>
2021-08-27 20:59:00 +0200

    removed a few nonsense items from list, see also #54

--- cookit/utils.py
+++ cookit/utils.py
@@ -1,17 +1,19 @@
+
+
 OIv4_FOOD_CLASSES = [
     'Apple', 'Artichoke', 'Asparagus', 'Bagel', 'Baked goods', 'Banana',
     'Beer', 'Bell pepper', 'Bread', 'Broccoli', 'Burrito', 'Cabbage', 'Cake',
-    'Candy', 'Cantaloupe', 'Carrot', 'Cheese', 'Chicken', 'Chopsticks',
+    'Candy', 'Cantaloupe', 'Carrot', 'Cheese', 'Chicken',
     'Coconut', 'Coffee', 'Cookie', 'Crab', 'Cream', 'Croissant', 'Cucumber',
     'Dairy', 'Dessert', 'Doughnut', 'Duck', 'Egg', 'Fast food', 'Fish', 'Food',
-    'Food processor', 'French fries', 'Fruit', 'Goose', 'Grape', 'Grapefruit',
+    'French fries', 'Fruit', 'Goose', 'Grape', 'Grapefruit',
     'Guacamole', 'Hamburger', 'Honeycomb', 'Hot dog', 'Ice cream', 'Jellyfish',
     'Juice', 'Lemon', 'Lobster', 'Mammal', 'Mango', 'Maple', 'Milk', 'Muffin',
     'Mushroom', 'Orange', 'Oyster', 'Pancake', 'Pasta', 'Pastry', 'Peach',
     'Pear', 'Pineapple', 'Pizza', 'Plant', 'Pomegranate', 'Popcorn', 'Potato',
     'Pretzel', 'Pumpkin', 'Radish', 'Salad', 'Salt and pepper shakers',
     'Sandwich', 'Seafood', 'Shellfish', 'Shrimp', 'Snail', 'Strawberry',
-    'Submarine sandwich', 'Sushi', 'Taco', 'Tart', 'Tea', 'Tomato', 'Towel',
+    'Submarine sandwich', 'Sushi', 'Taco', 'Tart', 'Tea', 'Tomato',
     'Turkey', 'Vegetable', 'Waffle', 'Watermelon', 'Whisk', 'Wine',
     'Winter melon', 'Zucchini'
 ]
