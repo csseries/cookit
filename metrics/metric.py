848781b7b40dbce30208d8017dfd2c5865894ec9
Justin Sams <justinsams98@yahoo.de>
2021-08-30 08:33:16 +0200

    relative paths

--- metrics/metric.py
+++ metrics/metric.py
@@ -16,7 +16,7 @@ def retrieve_info_from_csv():
 
     counter = 0
 
-    with open('/home/justin/code/JustinSms/cookit/cookit/metrics/food_testing_set.csv') as csvfile:
+    with open('metrics/food_testing_set.csv') as csvfile:
         read = csv.reader(csvfile, delimiter=";")
         for row in read:
             csv_list.append(row)
@@ -36,14 +36,14 @@ def retrieve_info_from_csv():
     return ingredients_dict
 
 
-def download_test_images(ingredients_dict):
+def download_test_images():
     counter = 0
-    #ingredients_dict = retrieve_info_from_csv()
+    ingredients_dict = retrieve_info_from_csv()
 
     for url in ingredients_dict.values():
         image = requests.get(url[1]).content
 
-        with open(f'images_from_csv/{counter}.jpg', 'wb') as writer:
+        with open(f'metrics/images_from_csv/{counter}.jpg', 'wb') as writer:
             writer.write(image)
 
     counter += 1
@@ -55,7 +55,7 @@ def making_prediction():
     img_sort_list = []
     sorted_img_list = []
 
-    for pic in os.listdir("/home/justin/code/JustinSms/cookit/cookit/metrics/images_from_csv"):
+    for pic in os.listdir("metrics/images_from_csv"):
 
         if pic == ".ipynb_checkpoints":
             pass
@@ -71,7 +71,7 @@ def making_prediction():
 
     for img in sorted_img_list:
         try:
-            prediction = predictor.predict(f"/home/justin/code/JustinSms/cookit/cookit/metrics/images_from_csv/{img}")
+            prediction = predictor.predict(f"metrics/images_from_csv/{img}")
             prediction_dict[f"{counter}"] = prediction
             counter += 1
         except:
@@ -94,25 +94,19 @@ def calculating_score():
     volumne_pred_counter = 0
 
     while counter <= 65:
-        #print(counter, "counter")
 
         for items in ingredients_dict[f"{counter}"][0]:
-            #print(items, "items")
             number_words = items.count(",") + 1
 
             volumne_counter += number_words
-            #print(volumne_counter, "volumne counter")
 
             for pred in prediction_dict[f"{counter}"]:
                 pred = pred.lower()
                 volumne_pred_counter += 1
-                #print(pred, "pred")
 
                 if pred in items:
-                    #print(pred, "pred_in_counter")
                     correct_counter += 1
                 else:
-                    #print(pred, "pred_fals_predict")
                     false_predict_counter += 1
 
         counter += 1
@@ -131,5 +125,5 @@ def calculating_score():
 
 #retrieve_info_from_csv()
 #download_test_images()
-#making_prediction()
-calculating_score()
+making_prediction()
+#calculating_score()
