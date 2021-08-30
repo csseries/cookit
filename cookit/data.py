cc57c7c4b6144888e5526bbf61b9a3c4d1d4e046
Michael Weitnauer <michael.weitnauer@gmail.com>
2021-08-30 20:59:46 +0200

    fix: variable name

--- cookit/data.py
+++ cookit/data.py
@@ -30,7 +30,7 @@ def get_oi_dataset_df(path='oi_food.csv', nrows=1000):
 
 # Should we perhaps keep non-food-related labels in the test set?
 def convert_oi_metadata(labelfile_path, baseurl='gs://somewhere', csv_path='tf_training.csv',
-                        test_split=0.2, val_split=0.1, classes=OIv4_INGREDIENTS_ONLY):
+                        test_split=0.2, val_split=0.1, food_classes=OIv4_INGREDIENTS_ONLY):
     """ Converts a json file in format fiftyone.types.FiftyOneImageDetectionDataset
         to a format as it is expected by the tflite_model_maker.object_detector
 
@@ -63,7 +63,7 @@ def convert_oi_metadata(labelfile_path, baseurl='gs://somewhere', csv_path='tf_t
             uuid_count += 1
             for label_items in labels:
                 label = classes[label_items['label']]
-                if label in classes:
+                if label in food_classes:
                     food_classes.append(label)
                     bbox_count += 1
                     bb = label_items['bounding_box']
