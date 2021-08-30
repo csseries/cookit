ba56072a1f51df8b2face1554325fe4495fe2a82
Michael Weitnauer <michael.weitnauer@gmail.com>
2021-08-30 13:08:58 +0200

    fix tests

--- tests/test_predictor.py
+++ tests/test_predictor.py
@@ -3,19 +3,23 @@ import numpy
 
 
 predictor = Predictor()
-
+test_file = 'tests/images/food.jpg'
 
 def testing_load_img_shape():
-    placeholder = predictor.load_img("raw_data/test_pic.jpg").shape
-    assert predictor.load_img("raw_data/test_pic.jpg").shape == [placeholder[0], placeholder[1], 3]
+    placeholder = predictor.load_img(test_file).shape
+    assert predictor.load_img(test_file).shape == [placeholder[0], placeholder[1], 3] # this doesn't make much sense, does it?
 
 
 def testing_predict():
-    assert type(predictor.predict("raw_data/test_pic.jpg")) == list
-    assert type(predictor.predict("raw_data/test_pic.jpg")[0]) == str
+    res = predictor.predict(test_file)
+    assert type(res) == tuple
+    assert type(res[0]) == list
+    assert type(res[0][0]) == str
+    # TODO: test returned scores and bounding boxes too
 
 
 def testing_run_detector():
-    assert type(predictor.run_detector("raw_data/test_pic.jpg")) == tuple
-    assert type(predictor.run_detector("raw_data/test_pic.jpg")[0][0]) == bytes
-    assert type(predictor.run_detector("raw_data/test_pic.jpg")[1][0]) == numpy.float32
+    res = predictor.run_detector(test_file)
+    assert type(res) == tuple
+    assert type(res[0][0]) == bytes
+    assert type(res[1][0]) == numpy.float32
